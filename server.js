0b041ece66fa424d0749516c74e72894d1f62d74
Sara Veber <53233521+saraveber@users.noreply.github.com>
2023-11-06 14:37:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const { BasicAuthenticator } = require('ibm-cloud-sdk-core');
 const uuid = require('uuid');
 
 // parse application/x-www-form-urlencoded
+//edit
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
